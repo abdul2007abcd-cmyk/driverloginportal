12d37de591ae930c558034cb5938c4de6c859218
abdul2007abcd-cmyk <abdul2007abcd@gmail.com>
2026-01-12 21:26:13 +0530

    Delete acting-driver directory

--- acting-driver/App.tsx
+++
@@ -1,3 +0,0 @@
-
-// This file is deprecated. See index.tsx for the vanilla implementation.
-export {};
--- acting-driver/firebase.ts
+++
@@ -1,3 +0,0 @@
-
-// This file is no longer used. The system has migrated to Supabase.
-export {};
